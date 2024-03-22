0605fbcb76f74f1724be19d87151e27c2f649f5d
Mridul chourasia <99160348+mridulchourasiya@users.noreply.github.com>
2024-03-22 19:23:19 +0530

    Setup = "Mongoose", "Express","cors"

---
+++ project 02/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, // replace *
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+// parse cookies into req.cookies
+
+export { app };
